2a7438bfa0c5912f0d46d834041efd8e7776eaf7
mdolci <mdolci@mwprog.ch>
2025-11-21 17:30:33 +0100

    add test

---
+++ cockpit/server.js
@@ -0,0 +1,20 @@
+﻿const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+console.log("Serveur WebSocket en ligne sur ws://localhost:8080");
+
+wss.on("connection", ws => {
+    console.log("Client connecté");
+
+    ws.on("message", message => {
+        wss.clients.forEach(client => {
+            if (client.readyState === 1 && client !== ws) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("Client déconnecté");
+    });
+});
