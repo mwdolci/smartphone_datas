5482ae966b500659a16e13e5c7f64678621919f6
Marco Dolci <marco.dolci@he-arc.ch>
2025-11-23 17:30:10 +0100

    update

--- cockpit/server.js
+++ cockpit/server.js
@@ -1,25 +1,23 @@
-﻿import WebSocket from "ws";
+﻿import WebSocket, { WebSocketServer } from "ws";
 
 const PORT = process.env.PORT || 10000;
-const wss = new WebSocket.Server({ port: PORT });
+const wss = new WebSocketServer({ port: PORT });
 
 console.log(`WebSocket server running on port ${PORT}`);
 
 wss.on("connection", ws => {
     console.log("Client connecté");
-
-    // Envoyer un message de bienvenue
+	
+	// Envoyer un message de bienvenue
     ws.send(JSON.stringify({ msg: "Bienvenue !" }));
 
     ws.on("message", message => {
-        // Convertir le Buffer en string si nécessaire
-        const msgStr = message.toString(); 
+        const msgStr = message.toString();
         console.log("Message reçu :", msgStr);
 
-        // Renvoyer à tous les autres clients
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN && client !== ws) {
-                client.send(msgStr); // on renvoie bien une string JSON
+                client.send(msgStr);
             }
         });
     });
